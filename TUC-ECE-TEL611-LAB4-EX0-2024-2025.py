ec5b4c843862d11f9565d6cb7d4034b185038f6e
Dermitzakis Eleftherios <eleftherios.dermitzakis@gmail.com>
2024-12-26 16:53:49 +0200

    Update TUC-ECE-TEL611-LAB4-EX0-2024-2025.py

--- TUC-ECE-TEL611-LAB4-EX0-2024-2025.py
+++ TUC-ECE-TEL611-LAB4-EX0-2024-2025.py
@@ -1,3 +1,34 @@
+"""
+File Name: least_squares_error_1d.py
+Author: Dr. Dermitzakis Emm. Eleftherios
+Date: 2024-12-26
+Version: 1.0
+
+Description:
+    This script demonstrates the application of Least Squares Error (LSE) to fit a linear model to a set of 1D data points. 
+    The script computes the best-fit line using the Least Squares method, which minimizes the sum of squared differences 
+    between the observed data points and the predicted values from the linear model. It then visualizes the data points 
+    and the best-fit line on a plot.
+
+    Key Components:
+    - Dataset: The script uses a set of 1D data points (x, y) to demonstrate the application of the LSE method.
+    - Linear Model Fitting: The slope (m) and intercept (b) of the best-fit line are calculated using the Least Squares method.
+    - Visualization: The data points and the resulting best-fit line are plotted for visualization.
+
+Usage:
+    - This script can be used to demonstrate the application of Least Squares Error to find the best-fit line for 1D data.
+    - The results will be displayed as a plot showing the data points and the corresponding best-fit line.
+
+Dependencies:
+    - numpy
+    - matplotlib
+
+Instructions:
+    - Ensure that the required dependencies (NumPy, matplotlib) are installed in your Python environment.
+    - Run the script to calculate and visualize the best-fit line for the given data points.
+    - The results will be printed in the console and displayed in a plot with the data points and the best-fit line.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
